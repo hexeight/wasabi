6fa9d36092cb62753db2a93d08be6d5016d6d3bb
Omkar Khair <omkarkhair@gmail.com>
2017-06-11 22:19:22 +0530

    wasabi compile

--- wasabi.js
+++ wasabi.js
@@ -4,7 +4,7 @@ var program = require('commander');
 program
   .version('0.0.1')
   .command('init', 'Initialize wasabi project')
-  .command('build', 'Build the contracts')
+  .command('compile', 'Compile contracts and create JS ABI')
   .command('deploy', 'Publish contracts to network')
   .command('serve', 'Run app')
   .parse(process.argv);
