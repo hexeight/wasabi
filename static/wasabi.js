c6c007de8e2ca40373db6c2c04f5eecc5469bc2e
Omkar Khair <omkarkhair@gmail.com>
2017-07-16 20:20:02 +0530

    closure

--- static/wasabi.js
+++ static/wasabi.js
@@ -1,38 +1,40 @@
-function startApp () {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            var wasabi = JSON.parse(xmlHttp.responseText);
-            wasabi.forEach(function (contract, index) {
-                var definition = web3.eth.contract(contract.abi);
-                var c = definition.at(contract.address);
-                window[contract.name] = {
-                    deployed: function () {
-                        return c;
-                    },
-                    at: function (address) {
-                        return definition.at(address);
-                    }
-                };
-            });
+(function () {
+    function startApp () {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                var wasabi = JSON.parse(xmlHttp.responseText);
+                wasabi.forEach(function (contract, index) {
+                    var definition = web3.eth.contract(contract.abi);
+                    var c = definition.at(contract.address);
+                    window[contract.name] = {
+                        deployed: function () {
+                            return c;
+                        },
+                        at: function (address) {
+                            return definition.at(address);
+                        }
+                    };
+                });
+            }
         }
+        xmlHttp.open("GET", "/contracts.json", true);
+        xmlHttp.send(null);
     }
-    xmlHttp.open("GET", "/contracts.json", true);
-    xmlHttp.send(null);
-}
 
-window.addEventListener('load', function() {
-    window.addEventListener('web3-loaded', startApp());
-    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof web3 !== 'undefined') {
-        // Use Mist/MetaMask's provider
-        window.web3 = new Web3(web3.currentProvider);
-        var event = new Event('web3-loaded');
-        window.dispatchEvent(event);
-    } else {
-        console.error("Web3 is undefined: Your browser does not support dApps. Please use Mist, Metamask of comparable dApp explorer.");
-        var event = new Event('web3-failed');
-        window.dispatchEvent(event);
-        return;
-    }
-});
+    window.addEventListener('load', function() {
+        window.addEventListener('web3-loaded', startApp());
+        // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+        if (typeof web3 !== 'undefined') {
+            // Use Mist/MetaMask's provider
+            window.web3 = new Web3(web3.currentProvider);
+            var event = new Event('web3-loaded');
+            window.dispatchEvent(event);
+        } else {
+            console.error("Web3 is undefined: Your browser does not support dApps. Please use Mist, Metamask of comparable dApp explorer.");
+            var event = new Event('web3-failed');
+            window.dispatchEvent(event);
+            return;
+        }
+    });
+})();
